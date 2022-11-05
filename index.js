2d69a6946a68dd3881f1bfd640b0c84c98c7515b
arfan36 <arfanur36@gmail.com>
2022-11-05 19:08:30 +0600

    68-2 Add data from JSON to database and load on the UI

--- index.js
+++ index.js
@@ -15,6 +15,25 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@clu
 // console.log('uri :>> ', uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run() {
+    try {
+        const productCollection = client.db('emaJohn').collection('products');
+
+        // Read (R)
+        app.get('/products', async (req, res) => {
+            const query = {};
+            const cursor = productCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        });
+
+    }
+    finally {
+
+    }
+}
+
+run().catch(err => console.log(err));
 
 app.get('/', (req, res) => {
     res.send('Hello from node mongo crud server');
