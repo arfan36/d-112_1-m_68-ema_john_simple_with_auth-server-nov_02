80a1fdbb356099d6ecd487a9550e22c2d5215950
arfan36 <arfanur36@gmail.com>
2022-11-06 11:35:45 +0600

    68-7 (Interesting) Load data based on the page number and size

--- index.js
+++ index.js
@@ -21,12 +21,12 @@ async function run() {
 
         // Read (R) all
         app.get('/products', async (req, res) => {
-            const page = req.query.page;
-            const size = req.query.size;
+            const page = parseInt(req.query.page);
+            const size = parseInt(req.query.size);
             console.log('page, size :>> ', page, size);
             const query = {};
             const cursor = productCollection.find(query);
-            const products = await cursor.toArray();
+            const products = await cursor.skip(page * size).limit(size).toArray();
             const count = await productCollection.estimatedDocumentCount();
             res.send({ count, products });
         });
